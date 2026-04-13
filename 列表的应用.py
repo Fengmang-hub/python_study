d7af0ac8a07519d305b4970420476f61b2f261ef
Fengmang-hub <3517714316@qq.com>
2026-04-13 21:09:06 +0800

    新添加列表的应用

---
+++ 列表的应用.py
@@ -0,0 +1,79 @@
+"""
+下面我们通过一个双色球随机选号的例子为大家讲解列表的应用。
+双色球是由中国福利彩票发行管理中心发售的乐透型彩票，每注投注号码由6个红色球和1个蓝色球组成。
+红色球号码从1到33中选择，蓝色球号码从1到16中选择。每注需要选择6个红色球号码和1个蓝色球号码。
+"""
+"""
+双色球随机选号程序
+"""
+
+"""
+import random
+
+red_balls = list(range(1, 34))
+selected_balls = []
+# 添加6个红色球到选中列表
+for _ in range(6):
+    # 生成随机整数代表选中的红色球的索引位置
+    index = random.randrange(len(red_balls))
+    # 将选中的球从红色球列表中移除并添加到选中列表
+    selected_balls.append(red_balls.pop(index))
+# 对选中的红色球排序
+selected_balls.sort()
+# 输出选中的红色球
+for ball in selected_balls:
+    print(f'\033[031m{ball:0>2d}\033[0m', end=' ')
+# 随机选择1个蓝色球
+blue_ball = random.randrange(1, 17)
+# 输出选中的蓝色球
+print(f'\033[034m{blue_ball:0>2d}\033[0m')
+"""
+
+"""
+双色球随机选号程序
+我们还可以利用random模块提供的sample和choice函数来简化上面的代码，前者可以实现无放回随机抽样，后者可以实现随机抽取一个元素，修改后的代码如下所示。
+"""
+"""
+import random
+
+red_balls = [i for i in range(1, 34)]
+blue_balls = [i for i in range(1, 17)]
+# 从红色球列表中随机抽出6个红色球（无放回抽样）
+selected_balls = random.sample(red_balls, 6)
+# 对选中的红色球排序
+selected_balls.sort()
+# 输出选中的红色球
+for ball in selected_balls:
+    print(f'\033[031m{ball:0>2d}\033[0m', end=' ')
+# 从蓝色球列表中随机抽出1个蓝色球
+blue_ball = random.choice(blue_balls)
+# 输出选中的蓝色球
+print(f'\033[034m{blue_ball:0>2d}\033[0m')
+"""
+
+
+"""
+双色球随机选号程序
+
+Author: 骆昊
+Version: 1.2
+"""
+"""
+import random
+
+n = int(input('生成几注号码: '))
+red_balls = [i for i in range(1, 34)]
+blue_balls = [i for i in range(1, 17)]
+for _ in range(n):
+    # 从红色球列表中随机抽出6个红色球（无放回抽样）
+    selected_balls = random.sample(red_balls, 6)
+    # 对选中的红色球排序
+    selected_balls.sort()
+    # 输出选中的红色球
+    for ball in selected_balls:
+        print(f'\033[031m{ball:0>2d}\033[0m', end=' ')
+    # 从蓝色球列表中随机抽出1个蓝色球
+    blue_ball = random.choice(blue_balls)
+    # 输出选中的蓝色球
+    print(f'\033[034m{blue_ball:0>2d}\033[0m')
+"""
