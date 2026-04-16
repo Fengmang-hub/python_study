81d96ae5e205d070ab41f7f8531931673188bfff
Fengmang-hub <3517714316@qq.com>
2026-04-16 20:41:01 +0800

    增添新练习

--- 列表 + 循环 + 分支综合.py
+++ 列表 + 循环 + 分支综合.py
@@ -1,3 +1,4 @@
+"""
 t = (10, 20, 30, 40, 50)
 a = list(t)
 print(a)
@@ -6,3 +7,15 @@ a.insert(2,99)
 print(a)
 b = tuple(a)
 print(b)
+"""
+
+"""
+num = 1
+list = []
+while num<=100:
+    list.append(num)
+    num+=1
+total = sum(list)
+print(list)
+print(total)
+"""
