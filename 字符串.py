ada0745f5cc2c2780d6286e080344f1355ffd4b7
Fengmang-hub <3517714316@qq.com>
2026-04-19 19:49:07 +0800

    补充完整

--- 字符串.py
+++ 字符串.py
@@ -127,3 +127,97 @@ print(s2.lower())       # goodbye
 print(s1)               # hello, world
 print(s2)               # GOODBYE
 '''
+
+'''
+find方法找不到指定的字符串会返回-1，index方法找不到指定的字符串会引发ValueError错误。
+'''
+'''
+s = 'hello, world!'
+print(s.find('or'))      # 8
+print(s.find('or', 9))   # -1
+print(s.find('or',8))
+print(s.find('of'))      # -1
+print(s.index('or'))     # 8
+print(s.index('or', 9))  # ValueError: substring not found
+'''
+
+'''
+s = 'hello world!'
+print(s.find('o'))       # 4
+print(s.rfind('o'))      # 7
+print(s.rindex('o'))     # 7
+# print(s.rindex('o', 8))  # ValueError: substring not found
+'''
+
+'''
+上面的isdigit用来判断字符串是不是完全由数字构成的
+isalpha用来判断字符串是不是完全由字母构成的，这里的字母指的是 Unicode 字符但不包含 Emoji 字符
+isalnum用来判断字符串是不是由字母和数字构成的。
+'''
+'''
+s1 = 'hello, world!'
+print(s1.startswith('He'))   # False
+print(s1.startswith('hel'))  # True
+print(s1.endswith('!'))      # True
+s2 = 'abc123456'
+print(s2.isdigit())  # False
+print(s2.isalpha())  # False
+print(s2.isalnum())  # True
+'''
+
+'''
+zfill 会识别正负号，把 0 补在符号和数字中间，而不是符号前面
+'''
+'''
+s = 'hello, world'
+print(s.center(20, '*'))  # ****hello, world****
+print(s.rjust(20))        #         hello, world
+print(s.ljust(20, '~'))   # hello, world~~~~~~~~
+print('33'.zfill(5))      # 00033
+print('-33'.zfill(5))     # -0033
+'''
+
+'''
+字符串的strip方法可以帮我们获得将原字符串修剪掉左右两端指定字符之后的字符串，默认是修剪空格字符。
+'''
+
+'''
+s1 = '   china666  '
+print(s1)
+print(s1.strip())      # china666
+s2 = '~你好，世界~'
+print(s2.lstrip('~'))  # 你好，世界~
+print(s2.rstrip('~'))  # ~你好，世界
+'''
+
+'''
+s = 'hello, good world'
+print(s.replace('o', '@'))     # hell@, g@@d w@rld
+print(s.replace('o', '@', 1))  # hell@, good world
+'''
+
+'''
+split方法默认使用空格进行拆分，我们也可以指定其他的字符来拆分字符串，而且还可以指定最大拆分次数来控制拆分的效果，代码如下所示。
+'''
+
+'''
+s = 'I love you'
+words = s.split()
+print(words)            # ['I', 'love', 'you']
+print('~'.join(words))  # I~love~you
+s = 'I#love#you#so#much'
+words = s.split('#')
+print(words)  # ['I', 'love', 'you', 'so', 'much']
+words = s.split('#', 2)
+print(words)  # ['I', 'love', 'you#so#much']
+'''
+
+'''
+a = '映枝'
+b = a.encode('utf-8')
+c = a.encode('gbk')
+print(b)                  # b'\xe6\x98\xa0\xe6\x9e\x9d'
+print(c)                  # b'\xd3\xb3\xd6\xa6'
+print(b.decode('utf-8'))  # 映枝
+print(c.decode('gbk'))    # 映枝
+'''
